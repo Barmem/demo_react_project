3853c1825f3b9c30348fc1798507150230176c0d
barmem <igor.neim7@gmail.com>
2023-04-05 10:51:36 +0600

    added restricted input and password with displayed text on click

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
+
 
 function App() {
+  const [message, setMessage] = useState('');
+  const handleChange = event => {
+    const result = event.target.value.replace(/[^a-z,0-9]/gi, '');
+    setMessage(result);
+  };
+  const [isShown, setIsSHown] = useState(false);
+  const togglePassword = () => {
+    setIsSHown((isShown) => !isShown);
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input id="message" name="message" type="text" value={message} onChange={handleChange}/>
+        <input id="message" name="message" type={isShown ? "text" : "password"}/>
+        <input id="checkbox" type="checkbox" checked={isShown} onChange={togglePassword}/>
       </header>
     </div>
   );
